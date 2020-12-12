570c2bbf26fc9c67e6639a8326aea0123ffed20a
Nicolaas on Lappy <nt@sunnysideup.co.nz>
2020-12-13 10:12:58 +1300

    MINOR: build

---
+++ src/js/toc.js
@@ -0,0 +1,31 @@
+
+const toc = () => {
+  // create the container div
+  const body = document.querySelector('body')
+  // get all divs
+  const headings = document.querySelectorAll('.typography h1, .typography h2')
+  // get the body element
+  // apply class to container div
+  if (headings.length > 6) {
+    body.classList.add('has-toc')
+    body.classList.add('toc-off')
+    for (let i = 0; i < headings.length; i++) {
+      const el = headings[i]
+      el.id = 'toc-' + i
+      el.addEventListener(
+        'click',
+        function (e) {
+          body.classList.toggle('toc-on')
+          if (body.classList.contains('toc-on') === false) {
+            window.location.hash = this.id
+          }
+        },
+        false
+      )
+    }
+  } else {
+    // body.classList.add('no-toc')
+  }
+}
+
+toc()
