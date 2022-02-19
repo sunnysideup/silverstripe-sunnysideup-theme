3885c29966366b7acb1275d0d9f86b4e0ac666a3
Nicolaas / Sunn Side Up <github@sunnysideup.co.nz>
2022-02-20 11:58:13 +1300

    Update body-class.js

--- src/js/body-class.js
+++ src/js/body-class.js
@@ -39,6 +39,7 @@ const bodyClass = {
       'DOMContentLoaded',
       function (event) {
         bodyClass.bodyObject.classList.add('body-loaded')
+        bodyClass.bodyObject.classList.remove('body-unloaded')
         if ('ontouchstart' in document.documentElement) {
           bodyClass.bodyObject.classList.add('touch')
         } else {
