4e980a7e8a53490d904989f23e07b4595fce6ab9
Nicolaas <github@sunnysideup.co.nz>
2023-12-01 10:36:17 +1300

    WIP

--- src/js/image-hover.js
+++ src/js/image-hover.js
@@ -1,19 +1,28 @@
-const heartbeat = {
+const imagehover = {
+    resetTimeout: null,
+
     init: function () {
         document.querySelectorAll('.image-container').forEach(el => {
             el.addEventListener('mousemove', e => {
+                clearTimeout(this.resetTimeout)
+
                 const { width, height, left, top } =
                     e.target.getBoundingClientRect()
                 const x = e.pageX - left - window.scrollX
                 const y = e.pageY - top - window.scrollY
+
                 e.target.style.setProperty('--mouse-x', (x / width) * 50 - 25)
                 e.target.style.setProperty('--mouse-y', 25 - (y / height) * 50)
+
+                this.resetTimeout = setTimeout(() => {
+                    e.target.style.removeProperty('--mouse-x')
+                    e.target.style.removeProperty('--mouse-y')
+                }, 1000)
             })
         })
     }
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Your code goes here
-    heartbeat.init()
+    imagehover.init()
 })
