7f92a6db1d510d3abd9e8b7041c4fffe4034f008
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-12-28 15:03:54 +1300

    FIX: BUILD

--- src/js/image-hover.js
+++ src/js/image-hover.js
@@ -2,28 +2,38 @@ const imagehover = {
     resetTimeout: null,
 
     init: function () {
-        document.querySelectorAll('.image-container').forEach(el => {
-            el.addEventListener('mousemove', e => {
-                if (this.isTouchDevice()) {
-                    clearTimeout(this.resetTimeout)
-                }
+        document
+            .querySelectorAll(
+                '.element sunnysideup__app__elements__workexample .image-container'
+            )
+            .forEach(el => {
+                el.addEventListener('mousemove', e => {
+                    if (this.isTouchDevice()) {
+                        clearTimeout(this.resetTimeout)
+                    }
 
-                const { width, height, left, top } =
-                    e.target.getBoundingClientRect()
-                const x = e.pageX - left - window.scrollX
-                const y = e.pageY - top - window.scrollY
+                    const { width, height, left, top } =
+                        e.target.getBoundingClientRect()
+                    const x = e.pageX - left - window.scrollX
+                    const y = e.pageY - top - window.scrollY
 
-                e.target.style.setProperty('--mouse-x', (x / width) * 50 - 25)
-                e.target.style.setProperty('--mouse-y', 25 - (y / height) * 50)
+                    e.target.style.setProperty(
+                        '--mouse-x',
+                        (x / width) * 50 - 25
+                    )
+                    e.target.style.setProperty(
+                        '--mouse-y',
+                        25 - (y / height) * 50
+                    )
 
-                if (this.isTouchDevice()) {
-                    this.resetTimeout = setTimeout(() => {
-                        e.target.style.removeProperty('--mouse-x')
-                        e.target.style.removeProperty('--mouse-y')
-                    }, 1000)
-                }
+                    if (this.isTouchDevice()) {
+                        this.resetTimeout = setTimeout(() => {
+                            e.target.style.removeProperty('--mouse-x')
+                            e.target.style.removeProperty('--mouse-y')
+                        }, 1000)
+                    }
+                })
             })
-        })
     },
     isTouchDeviceVar: null,
 
